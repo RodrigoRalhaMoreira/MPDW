7f378a9ffb6cde8b1035daadbb45cf06ef0119ca
joaoncfsantos <santos.joaonuno2000@gmail.com>
2023-03-29 10:42:25 +0100

    ifetch already recommends cloth by color

---
+++ opensearchData.py
@@ -0,0 +1,83 @@
+import pprint as pp
+import requests
+import json
+from tqdm import tqdm
+import pprint as pp
+from opensearchpy import OpenSearch
+from opensearchpy import helpers
+from PIL import Image
+import requests
+import pandas as pd
+import time
+import numpy as np
+
+
+import response_generate as rr
+
+from transformers import CLIPProcessor, CLIPModel
+
+host = 'api.novasearch.org'
+port = 443
+
+index_name = "farfetch_images"
+
+user = 'ifetch' # Add your user name here.
+password = 'S48YdnMQ' # Add your user password here. For testing only. Don't store credentials in code.
+
+
+client = OpenSearch(
+    hosts = [{'host': host, 'port': port}],
+    http_compress = True,
+    http_auth = (user, password),
+    url_prefix = 'opensearch',
+    use_ssl = True,
+    verify_certs = False,
+    ssl_assert_hostname = False,
+    ssl_show_warn = False
+)
+
+if client.indices.exists(index = index_name):
+
+    print('\n----------------------------------------------------------------------------------- INDEX SETTINGS')
+    settings = client.indices.get_settings(index = index_name)
+    pp.pprint(settings)
+
+    print('\n----------------------------------------------------------------------------------- INDEX MAPPINGS')
+    mappings = client.indices.get_mapping(index = index_name)
+    pp.pprint(mappings)
+
+    print('\n----------------------------------------------------------------------------------- INDEX #DOCs')
+    print(client.count(index = index_name))
+
+else:
+    print("Index not found")
+
+
+def searchRawInfo(qtxt):
+  query_denc = {
+    'size': 3, #how many products we want
+    '_source': ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender', 
+                'product_main_colour', 'product_second_color', 'product_brand', 'product_materials', 
+                'product_short_description', 'product_attributes', 'product_image_path', 
+                'product_highlights', 'outfits_ids', 'outfits_products'],
+    'query': {
+      'multi_match': {
+        'query': qtxt,
+        'fields': ['product_main_colour']
+      }
+    }
+  }
+
+  response = client.search(
+      body = query_denc,
+      index = index_name
+  )
+
+  results = [r['_source'] for r in response['hits']['hits']]
+  print('\nSearch results:')
+  results
+
+
+  pp.pprint(response)
+  return response
+
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import json
 
 import response_generate as r
+import opensearchData
 
 app = Flask(__name__) # create the Flask app
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -18,98 +19,12 @@ def dialog_turn():
         print(data.get('session_id'))
         print(data.get('user_action'))
         print(data.get('interface_selected_product_id'))
-        print(data.get('image'))
+        print(data.get('image'))   
 
-        response = {'_shards': {'failed': 0, 'skipped': 0, 'successful': 4, 'total': 4},
-                    'hits': {'hits': [{'_id': '17872033',
-                                       '_index': 'farfetch_images',
-                                       '_score': 1.3735868,
-                                       '_source': {'outfits_ids': [76013],
-                                                   'outfits_products': [[14592496,
-                                                                         15953626,
-                                                                         17872033,
-                                                                         17872047]],
-                                                   'product_attributes': '[{"attribute_name":"Neckline","attribute_values":["Round '
-                                                                         'Neck"]},{"attribute_name":"Sleeve '
-                                                                         'Length","attribute_values":["Shortsleeved"]}]',
-                                                   'product_brand': 'ROBERTO CAVALLI',
-                                                   'product_category': 'T-Shirts & Vests',
-                                                   'product_family': 'Clothing',
-                                                   'product_gender': 'MEN',
-                                                   'product_highlights': '[black, cotton, patch '
-                                                                         'detail, logo patch to '
-                                                                         'the front, crew neck, '
-                                                                         'short sleeves]',
-                                                   'product_id': 17872033,
-                                                   'product_image_path': 'https://large.novasearch.org/farfetch_products/images/17/87/20/33/17872033.jpg',
-                                                   'product_main_colour': 'BLACK',
-                                                   'product_materials': ['Cotton'],
-                                                   'product_second_color': 'N/D',
-                                                   'product_short_description': 'patch-detail '
-                                                                                'T-shirt',
-                                                   'product_sub_category': 'T-Shirts'},
-                                       '_type': '_doc'},
-                                      {'_id': '17888647',
-                                       '_index': 'farfetch_images',
-                                       '_score': 1.3735868,
-                                       '_source': {'outfits_ids': [],
-                                                   'outfits_products': [],
-                                                   'product_attributes': '[{"attribute_name":"Neckline","attribute_values":["Round '
-                                                                         'Neck"]},{"attribute_name":"Sleeve '
-                                                                         'Length","attribute_values":["Shortsleeved"]}]',
-                                                   'product_brand': 'A.P.C.',
-                                                   'product_category': 'T-Shirts & Vests',
-                                                   'product_family': 'Clothing',
-                                                   'product_gender': 'MEN',
-                                                   'product_highlights': '[black\xa0, cotton\xa0, '
-                                                                         'jersey knit, logo print '
-                                                                         'to the front, crew '
-                                                                         'neck, short sleeves, '
-                                                                         'straight hem]',
-                                                   'product_id': 17888647,
-                                                   'product_image_path': 'https://large.novasearch.org/farfetch_products/images/17/88/86/47/17888647.jpg',
-                                                   'product_main_colour': 'BLACK',
-                                                   'product_materials': ['Cotton'],
-                                                   'product_second_color': 'N/D',
-                                                   'product_short_description': 'logo-print '
-                                                                                'cotton T-Shirt',
-                                                   'product_sub_category': 'T-Shirts'},
-                                       '_type': '_doc'},
-                                      {'_id': '17905525',
-                                       '_index': 'farfetch_images',
-                                       '_score': 1.3735868,
-                                       '_source': {'outfits_ids': [74297],
-                                                   'outfits_products': [[14812812,
-                                                                         17477049,
-                                                                         17902341,
-                                                                         17905525]],
-                                                   'product_attributes': '[{"attribute_name":"Neckline","attribute_values":["Round '
-                                                                         'Neck"]},{"attribute_name":"Sleeve '
-                                                                         'Length","attribute_values":["Shortsleeved"]}]',
-                                                   'product_brand': 'DONDUP',
-                                                   'product_category': 'T-Shirts & Vests',
-                                                   'product_family': 'Clothing',
-                                                   'product_gender': 'MEN',
-                                                   'product_highlights': '[black/off white, '
-                                                                         'stretch-cotton, logo '
-                                                                         'print to the front, '
-                                                                         'round neck, short '
-                                                                         'sleeves, straight hem]',
-                                                   'product_id': 17905525,
-                                                   'product_image_path': 'https://large.novasearch.org/farfetch_products/images/17/90/55/25/17905525.jpg',
-                                                   'product_main_colour': 'BLACK',
-                                                   'product_materials': ['Spandex/Elastane',
-                                                                         'Cotton'],
-                                                   'product_second_color': 'N/D',
-                                                   'product_short_description': 'logo-print '
-                                                                                'short-sleeve '
-                                                                                'T-shirt',
-                                                   'product_sub_category': 'T-Shirts'},
-                                       '_type': '_doc'}],
-                             'max_score': 1.3735868,
-                             'total': {'relation': 'gte', 'value': 10000}},
-                    'timed_out': False,
-                    'took': 15}
+        userUtterance = data.get('utterance')
+        
+        response = opensearchData.searchRawInfo(userUtterance)
+
         if response['hits']['total']['value'] > 0:
             response_recommandations = r.response_to_recommandations(response)
             response_prompt = "Here's what I found for you:\n test"
